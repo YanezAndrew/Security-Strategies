e76900166b3745f0f95c071768df07f309ab7eb6
YanezAndrew <andrewyanez056@gmail.com>
2024-02-11 00:25:21 -0800

    Starter Code

---
+++ ComputeValue.py
@@ -0,0 +1,20 @@
+'''
+Skeleton code for finding security strategies.
+Please do not change the filename or the function names!
+'''
+
+import numpy as np
+
+def ComputeValue(M):
+    '''
+    Computes the value and security strategies of a two-player, two-move, zero-sum game. Refer to the HW assignment for details on P1, P2, V1, V2, and M.
+    '''
+
+    ### WRITE YOUR CODE BELOW
+    P1 = None
+    P2 = None
+    V1 = None
+    V2 = None
+
+    ### DO NOT EDIT BELOW THIS LINE
+    return P1, P2, V1, V2
